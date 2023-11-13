31bd227237ffa6a27b235eb54b2e9ac79fc0f55c
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-11-13 13:45:39 +0300

    Added entities

---
+++ TaskGauge/TaskGauge.Entity/Entity/Task.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskGauge.Entity.Entity
+{
+    [Table("Task")]
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int RoomId { get; set; }
+        public DateTime RecordDate { get; set; } = DateTime.Now;
+        public DateTime? ModifiedDate { get; set; } 
+        public int? ModifiedBy { get; set; } 
+        public int RecordBy { get; set; }
+        [ForeignKey("RecordBy")]
+        public virtual User RecordUser { get; set; }
+        [ForeignKey("ModifiedBy")]
+        public virtual User ModifiedUser { get; set; }
+        [ForeignKey("RoomId")]
+        public virtual Room Room { get; set; }
+    }
+}
