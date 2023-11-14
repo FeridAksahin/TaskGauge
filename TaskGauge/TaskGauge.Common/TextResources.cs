74d004a976538d43602f5b98b70ed4b835533b3c
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-11-14 20:03:01 +0300

    Added resource file

---
+++ TaskGauge/TaskGauge.Common/TextResources.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskGauge.Common
+{
+    public static class TextResources
+    {
+        public static readonly string UsernameExists = "The username entered already exists. Please enter another username."; 
+        public static readonly string SuccessfullyRegisteredUser = "You have registered."; 
+    }
+}
