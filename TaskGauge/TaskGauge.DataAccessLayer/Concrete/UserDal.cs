584355f13735ce1720e05bbf096188dc9e5c3662
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-11-14 20:13:12 +0300

    Added exception handling for general exception message to client

--- TaskGauge/TaskGauge.DataAccessLayer/Concrete/UserDal.cs
+++ TaskGauge/TaskGauge.DataAccessLayer/Concrete/UserDal.cs
@@ -21,29 +21,38 @@ namespace TaskGauge.DataAccessLayer.Concrete
 
         public string Register(RegisterDto registerDto)
         {
-            var isExistUsername = _taskGaugeContext.User
-                  .AsEnumerable()
-                  .Any(entity => entity.Name.Equals(registerDto.Username, StringComparison.OrdinalIgnoreCase));
-            if (isExistUsername)
+            try
             {
-                return $"{TextResources.UsernameExists} Type: error";
+                var isExistUsername = _taskGaugeContext.User
+                                  .AsEnumerable()
+                                  .Any(entity => entity.Name.Equals(registerDto.Username, StringComparison.OrdinalIgnoreCase));
+                if (isExistUsername)
+                {
+                    return $"{TextResources.UsernameExists} Type: error";
+                }
+
+                var securityQuestionEntity = (from entity in _taskGaugeContext.SecurityQuestion
+                                              where entity.Question == registerDto.SecurityQuestion
+                                              select new { entity.Id }).First();
+
+                var securityQuestionId = securityQuestionEntity != null ? securityQuestionEntity.Id : default(int);
+
+                _taskGaugeContext.User.Add(new User
+                {
+                    Name = registerDto.Username,
+                    Password = CryptoPassword.EncryptPassword(registerDto.Password),
+                    RecordTime = DateTime.Now,
+                    SecurityQuestionId = securityQuestionId,
+                    SecurityQuestionAnswer = registerDto.SecurityQuestionAnswer,
+                });
+                _taskGaugeContext.SaveChanges();
+                return $"{TextResources.SuccessfullyRegisteredUser} Type: success";
             }
-
-            var securityQuestionEntity = (from entity in _taskGaugeContext.SecurityQuestion
-                                      where entity.Question == registerDto.SecurityQuestion select new {entity.Id}).First();
-
-            var securityQuestionId = securityQuestionEntity != null ? securityQuestionEntity.Id : default(int);
-
-            _taskGaugeContext.User.Add(new User
+            catch(Exception exception)
             {
-                Name = registerDto.Username,
-                Password = CryptoPassword.EncryptPassword(registerDto.Password),
-                RecordTime = DateTime.Now,
-                SecurityQuestionId = securityQuestionId,
-                SecurityQuestionAnswer = registerDto.SecurityQuestionAnswer, 
-            });
-            _taskGaugeContext.SaveChanges();
-            return "";
+                return $"{exception.Message} Type: error";
+            }
+            
         }
     }
 }
