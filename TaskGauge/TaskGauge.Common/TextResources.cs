48a074982554522ee1486b24de00aecc2e6443ae
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-11-14 21:58:06 +0300

    Updated resource

--- TaskGauge/TaskGauge.Common/TextResources.cs
+++ TaskGauge/TaskGauge.Common/TextResources.cs
@@ -10,5 +10,7 @@ namespace TaskGauge.Common
     {
         public static readonly string UsernameExists = "The username entered already exists. Please enter another username."; 
         public static readonly string SuccessfullyRegisteredUser = "You have registered."; 
+        public static readonly string WrongPassword = "The entered password is incorrect."; 
+        public static readonly string WrongUsername = "The entered username is incorrect."; 
     }
 }
