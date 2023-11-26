f986179922140c37716a2497b7adecc417f79beb
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-11-27 02:57:55 +0300

    Room action and json method moved to Room controller

---
+++ TaskGauge/TaskGauge.Mvc/Controllers/RoomController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskGauge.Mvc.Controllers
+{
+    public class RoomController : Controller
+    {
+        RoomStatic roomUser = RoomStatic.Instance;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult NewRoom(string roomName)
+        {
+            return Json(null);
+        }
+    }
+}
