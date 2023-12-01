a98ed56f95c956d58aac80aa523404aba4eff566
Ferid Akşahin <ferid.aksahin98@gmail.com>
2023-12-01 12:01:33 +0300

    Updated hub

--- TaskGauge/TaskGauge.Mvc/Hubs/TaskGaugeHub.cs
+++ TaskGauge/TaskGauge.Mvc/Hubs/TaskGaugeHub.cs
@@ -7,7 +7,7 @@ namespace TaskGauge.Mvc.Hubs
 {
     public class TaskGaugeHub : Hub
     {
-        public static List<RoomUserDto> roomUserDto = new List<RoomUserDto>();
+        RoomStatic roomUser = RoomStatic.Instance;
         public async Task JoinRoom(string roomName)
         {
             var httpContext = Context.GetHttpContext();
@@ -16,15 +16,15 @@ namespace TaskGauge.Mvc.Hubs
             roomMember.IsItInTheRoom = true;
             roomMember.RoomName = roomName;
             roomMember.Username = username;
-            if (!roomUserDto.Exists(x => x.Username == username))
+            if (!roomUser.roomUser.Exists(x => x.Username == username))
             {
-                roomUserDto.Add(roomMember);
+                roomUser.roomUser.Add(roomMember);
                 await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
                 await Clients.OthersInGroup(roomName).SendAsync("userJoined", username);
             }
-            else if (roomUserDto.Exists(x => x.Username == username && x.IsItInTheRoom == false))
+            else if (roomUser.roomUser.Exists(x => x.Username == username && x.IsItInTheRoom == false))
             {
-                roomUserDto.ForEach(user => user.IsItInTheRoom = user.Username == username ? true : user.IsItInTheRoom);
+                roomUser.roomUser.ForEach(user => user.IsItInTheRoom = user.Username == username ? true : user.IsItInTheRoom);
                 await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
                 await Clients.OthersInGroup(roomName).SendAsync("userJoined", username);
             }
@@ -34,8 +34,8 @@ namespace TaskGauge.Mvc.Hubs
 
             var httpContext = Context.GetHttpContext();
             var username = httpContext.Session.GetString("Username");
-            var roomName = roomUserDto.FirstOrDefault(x => x.Username == username).RoomName;
-            roomUserDto.ForEach(x => x.IsItInTheRoom = x.Username == username ? false : x.IsItInTheRoom);
+            var roomName = roomUser.roomUser.FirstOrDefault(x => x.Username == username).RoomName;
+            roomUser.roomUser.ForEach(x => x.IsItInTheRoom = x.Username == username ? false : x.IsItInTheRoom);
             await Clients.OthersInGroup(roomName).SendAsync("userLeft", username);
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
             await base.OnDisconnectedAsync(exception);
