97f0e9661a56d48595f8d4d8bd1818b461d55689
Ferid Akşahin <ferid.aksahin98@gmail.com>
2024-02-22 21:40:44 +0300

    Remove unnecessary column from database

---
+++ TaskGauge/TaskGauge.Entity/Migrations/20240222183931_delete column.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskGauge.Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class deletecolumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Task_User_ModifiedBy",
+                table: "Task");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Task_ModifiedBy",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "ModifiedBy",
+                table: "Task");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ModifiedBy",
+                table: "Task",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Task_ModifiedBy",
+                table: "Task",
+                column: "ModifiedBy");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Task_User_ModifiedBy",
+                table: "Task",
+                column: "ModifiedBy",
+                principalTable: "User",
+                principalColumn: "Id");
+        }
+    }
+}
