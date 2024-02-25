2d32e50a4a8ba0a3d90e5fd4471f19e27937095f
Ferid Akşahin <ferid.aksahin98@gmail.com>
2024-02-25 16:48:36 +0300

    Added 'TotalEffort' column to 'RoomTaskInformation' entity with Code First

---
+++ TaskGauge/TaskGauge.Entity/Migrations/20240225134602_added totalEffort.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskGauge.Entity.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedtotalEffort : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TotalEffort",
+                table: "RoomTaskInformation",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalEffort",
+                table: "RoomTaskInformation");
+        }
+    }
+}
